d430aba2f16edf4c4f66f930e191a0d1508bdab0
Jason <alvarezcoraiza@citypolyhs.org>
2017-02-13 21:27:07 +0000

    very very fun

--- Script.js
+++ Script.js
@@ -1,11 +1,19 @@
+
+var password;
+password="ScriptEd";
+
+var guess;
+
 $(document).ready(function(){
-       
-   $("#show").hide();
-   $("#show").click(function(){
-    $("p").show();
-});
-   
-   
-   
-    
+ console.log("ready");
+
+   $("#submit").click(function(){
+       console.log("click");
+       guess = $('#words').val();
+        console.log(guess);
+        console.log(password);
+        if (password===guess) {
+            $('#after').append("WOOOOOOOHHHH!!111! You guessed the secret word!")
+        }
+   });
 });
